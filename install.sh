48a468a7007e1136dfffb30cd63bb1c301a053b7
210mlt <210mlt@gmail.com>
2022-09-07 21:47:57 +0200

    install fix

--- install.sh
+++ install.sh
@@ -4,7 +4,5 @@ for f in *
 do
   [ "$f" == "install.sh" ] || sed -i 's/{path}/'"${wpath}"'/g' "$f"
 done
-#echo "test{path}^^{path}exit" | sed 's/{path}/0pth0/g'
-exit
 cp youtubeurlplayer.desktop ~/.local/share/applications/
 xdg-mime default youtubeurlplayer.desktop x-scheme-handler/ytu
