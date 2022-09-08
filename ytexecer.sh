24ae232209aa22edb8d2f24cf069f506f41b3eb8
210mlt <210mlt@gmail.com>
2022-09-08 04:10:10 +0200

    updated

--- ytexecer.sh
+++ ytexecer.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
-#type=$(echo "${ytu}" | awk '{print $1}' FS='_')		-> $1
-#action=$(echo "${ytu}" | awk '{print $2}' FS='_')		-> $2
-#ytu=$(echo "${ytu}" | awk '{print $2}' FS="_${action}_")	-> $3
-ypath="~/project-pipe/"
+#type 		-> $1
+#action		-> $2
+#id/urlpart	-> $3
+ypath="/home/damian/project-pipe/"
+cd "$ypath"
+
 case "$1" in
 "v")
   case "$2" in
-#  "p")
   "v")
-    cd "$ypath"
     source sh/prcvideo.src
     prcvideo "${3}" "view+"
   ;;
   "vv")
-    cd "$ypath"
+    source sh/prcvideo.src
+    prcvideo "${3}" "view+{;}view{;}next"
+  ;;
+  "vvk")
     source sh/prcvideo.src
     prcvideo "${3}" "view+{;}view"
   ;;
   *)
-    cd "$ypath"
     source sh/prcvideo.src
     prcvideo "${3}"
   ;;
@@ -27,24 +29,41 @@ case "$1" in
 "a")
   case "$2" in
   "l")
-    cd "$ypath"
     source sh/prcvideo.src
     prcvideo "${3}" "view+{;}listen-gui{;}126 kbps{x}132459{x}M4A{;}next"
   ;;
   "lk")
-    cd "$ypath"
     source sh/prcvideo.src
     prcvideo "${3}" "view+{;}listen-gui{;}126 kbps{x}132459{x}M4A"
   ;;
   *)
-    cd "$ypath"
     source sh/prcvideo.src
-    prcvideo "${3}" "view+{;}listen"
+    prcvideo "${3}" "view+{;}listen-gui"
   ;;
   esac
 ;;
 "p")
+  case "$2" in
+  "v")
+    st -e ./playviewall.sh "playlists/byid/${3}";;
+  "l")
+    st -e ./playlistenall.sh "playlists/byid/${3}";;
+  *)
+    source sh/prcplaylist.src
+    prcplaylist "${3}"
+  ;;
+  esac
 ;;
 "c")
+  case "$2" in
+  "v")
+    st -e ./playviewall.sh "channel_ids/${3}";;
+  "l")
+    st -e ./playlistenall.sh "channel_ids/${3}";;
+  *)
+    source sh/prcchannel.src
+    prcchannel "${3}"
+  ;;
+  esac
 ;;
 esac
